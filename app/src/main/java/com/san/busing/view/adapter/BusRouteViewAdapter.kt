153de04aa0ac3ebd9527455d398be58f2b43e1e2
seongsan <tjdtks030@naver.com>
2024-05-18 07:02:48 +0900

    design: 노선 조회 결과 아이템 디자인 일부 수정

--- app/src/main/java/com/san/busing/view/adapter/BusRouteViewAdapter.kt
+++ app/src/main/java/com/san/busing/view/adapter/BusRouteViewAdapter.kt
@@ -9,10 +9,7 @@ import com.san.busing.domain.model.BusRouteModel
 class BusRouteViewAdapter(private val items: List<BusRouteModel>) : RecyclerView.Adapter<BusRouteViewAdapter.BusRouteViewHolder>() {
     inner class BusRouteViewHolder(private val binding: ItemBusRouteBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
-            binding.txtId.text = items[position].id.get().toString()
-            binding.txtName.text = items[position].name
-            binding.txtTypeCd.text = items[position].typeCd.get().toString()
-            binding.txtTypeName.text = items[position].typeName
+            binding.txtRouteName.text = items[position].name
             binding.txtRegion.text = items[position].region
         }
     }
