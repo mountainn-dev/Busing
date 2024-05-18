6bcf110e13f8bf7f84aba37c12f6ccfb35163b7f
seongsan <tjdtks030@naver.com>
2024-05-18 23:29:44 +0900

    feat: 노선 번호순으로 정렬된 검색 결과 출력

--- app/src/main/java/com/san/busing/domain/viewmodelimpl/SearchBusRouteViewModelImpl.kt
+++ app/src/main/java/com/san/busing/domain/viewmodelimpl/SearchBusRouteViewModelImpl.kt
@@ -32,7 +32,7 @@ class SearchBusRouteViewModelImpl(private val repository: BusRouteRepositoryImpl
     private suspend fun searchBusRoutes(keyword: String) {
         val result = repository.getBusRoutes(keyword)
         if (result is Success) {
-            content = result.data()
+            content = result.data().sortedBy { it.name }   // 노선 번호순 정렬
             contentReady.postValue(true)
         } else {
             error = (result as Error).message()
