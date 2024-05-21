6c1c12d8cc94e1f53148f8ac298a0c7d25d759b2
seongsan <tjdtks030@naver.com>
2024-05-21 13:13:25 +0900

    fix: 노선 검색 api 통신 과부하 방지
    SearchBusRouteViewModel 클래스 isLoading 변수 작성 및 search() 수정

--- app/src/main/java/com/san/busing/domain/viewmodelimpl/SearchBusRouteViewModelImpl.kt
+++ app/src/main/java/com/san/busing/domain/viewmodelimpl/SearchBusRouteViewModelImpl.kt
@@ -15,30 +15,36 @@ import kotlinx.coroutines.launch
 class SearchBusRouteViewModelImpl(private val repository: BusRouteRepositoryImpl) : SearchViewModel, ViewModel() {
     var keyword = ""
     var content = listOf<BusRouteModel>()
-    private val searchCompleted = MutableLiveData<Boolean>()
+    private val contentLoaded = MutableLiveData<Boolean>()
+    private var isLoading = false
     private var error = ""
 
     override val contentReady: LiveData<Boolean>
-        get() = searchCompleted
+        get() = contentLoaded
 
     override fun search(keyword: String) {
-        this.keyword = keyword
+        if (!isLoading) {
+            isLoading = true
+            this.keyword = keyword
 
-        viewModelScope.launch {
-            searchBusRoutes(keyword)
+            viewModelScope.launch {
+                searchBusRoutes(keyword)
+                isLoading = false
+            }
         }
     }
 
     private suspend fun searchBusRoutes(keyword: String) {
         val result = repository.getBusRoutes(keyword)
+
         if (result is Success) {
             // 검색 결과 출력 시 노선 번호, 운행 지역 순으로 출력
             content = result.data().sortedWith(compareBy({it.name}, {it.region}))
-            searchCompleted.postValue(true)
+            contentLoaded.postValue(true)
         } else {
             error = (result as Error).message()
             Log.e("BusRoute Exception", error)
-            searchCompleted.postValue(false)
+            contentLoaded.postValue(false)
         }
     }
 }
