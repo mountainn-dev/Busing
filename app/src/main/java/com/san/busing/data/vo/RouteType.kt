9c70925dbc283ce614aa2e8766132eaebed5d45f
seongsan <tjdtks030@naver.com>
2024-05-27 20:45:23 +0900

    feat: 색상 지정을 위한 RouteType 유형별 체크 기능

--- app/src/main/java/com/san/busing/data/vo/RouteType.kt
+++ app/src/main/java/com/san/busing/data/vo/RouteType.kt
@@ -1,6 +1,7 @@
 package com.san.busing.data.vo
 
 import com.san.busing.domain.enums.RouteType
+import com.san.busing.domain.utils.Const
 
 data class RouteType(private val code: Int) {
     private lateinit var type: RouteType
@@ -11,8 +12,14 @@ data class RouteType(private val code: Int) {
     }
 
     private fun isRegisteredRouteType() = RouteType.entries.any { it.code == code }
-    private fun initRouteType() {
-        type = RouteType.entries.find { it.code == code }!!
-    }
+    private fun initRouteType() { type = RouteType.entries.find { it.code == code }!! }
+
     fun name() = type.tag
+    fun isAirport() = type.tag == Const.TAG_AIRPORT
+    fun isArea() = type.tag == Const.TAG_AREA
+    fun isCircular() = type.tag == Const.TAG_CIRCULAR
+    fun isNormal() = type.tag == Const.TAG_NORMAL
+    fun isOutTown() = type.tag == Const.TAG_OUT_TOWN
+    fun isRural() = type.tag == Const.TAG_RURAL
+    fun isVillage() = type.tag == Const.TAG_VILLAGE
 }
