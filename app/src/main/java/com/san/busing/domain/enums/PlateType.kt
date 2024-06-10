dd842e373d63e4ada3b74655a23497296cafe8ce
seongsan <tjdtks030@naver.com>
2024-06-10 22:47:09 +0900

    fix: '정보 없음' 에 대한 PlateType 추가

--- app/src/main/java/com/san/busing/domain/enums/PlateType.kt
+++ app/src/main/java/com/san/busing/domain/enums/PlateType.kt
@@ -8,6 +8,7 @@ import com.san.busing.domain.utils.Const
  * 경기도 공공 데이터 포털에서 제공하는 버스 차량 유형
  */
 enum class PlateType(val code: Int, val typeName: String, val tag: String) {
+    NONE(0, "정보없음", Const.TAG_NONE),
     COMPACT(1, "소형승합차", Const.TAG_COMPACT),
     MID_SIZE(2, "중형승합차", Const.TAG_MID_SIZE),
     FULL_SIZE(3, "대형승합차", Const.TAG_FULL_SIZE),
