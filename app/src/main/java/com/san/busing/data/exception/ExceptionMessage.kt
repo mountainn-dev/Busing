ff667272a0b0605be9a54d3874cb2e73bd19dc5d
seongsan <tjdtks030@naver.com>
2024-06-14 23:01:05 +0900

    fix: XmlParser Exception 핸들링 및 NO_SERVICE_RESULT_CODE ExceptionMessage 작성

--- app/src/main/java/com/san/busing/data/exception/ExceptionMessage.kt
+++ app/src/main/java/com/san/busing/data/exception/ExceptionMessage.kt
@@ -7,10 +7,10 @@ object ExceptionMessage {
     const val TAG_BUS_ROUTE_BUS_EXCEPTION = "Bus route Buses Exception"
     const val TAG_RECENT_SEARCH_EXCEPTION = "Recent search Exception"
 
+    const val NO_SERVICE_RESULT_CODE = "서비스 결과 코드가 존재하지 않습니다."
     const val WRONG_ID_FORMAT_EXCEPTION = "id 형식이 올바르지 않습니다."
     const val NO_ROUTE_TYPE_EXCEPTION = "노선 유형이 존재하지 않습니다."
     const val NO_PLATE_TYPE_EXCEPTION = "차량 유형이 존재하지 않습니다."
-    const val WRONG_END_BUS_VALUE_EXCEPTION = "막차 여부 값이 올바르지 않습니다."
     const val WRONG_LOW_PLATE_VALUE_EXCEPTION = "저상 버스 여부 값이 올바르지 않습니다."
     const val WRONG_REMAIN_SEAT_VALUE_EXCEPTION = "차량 빈자리 수 값이 올바르지 않습니다."
     const val NO_SERVICE_RESULT = "서비스 결과값이 존재하지 않습니다."
--- app/src/main/java/com/san/busing/data/source/remote/interceptor/ErrorInterceptor.kt
+++ app/src/main/java/com/san/busing/data/source/remote/interceptor/ErrorInterceptor.kt
@@ -1,9 +1,10 @@
 package com.san.busing.data.source.remote.interceptor
 
-import android.util.Log
+import com.san.busing.data.exception.ExceptionMessage
+import com.san.busing.data.exception.ServiceException
 import com.san.busing.data.source.remote.parser.XmlParser
-import com.san.busing.data.source.remote.retrofit.ServiceResult.*
-import com.san.busing.data.exception.ApiException
+import com.san.busing.data.source.remote.retrofit.ServiceResult.NO_RESULT
+import com.san.busing.data.source.remote.retrofit.ServiceResult.SUCCESS
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.InputStream
@@ -14,7 +15,6 @@ import java.io.InputStream
  * 서비스 API 로부터 응답을 받았을 때, 특정 상황의 경우 응답을 그대로 반환하지 않고 Exception 을 throw 하기 위한 클래스
  */
 class ErrorInterceptor : Interceptor {
-    @Throws(ApiException.NoResultException::class, Exception::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val response = chain.proceed(request)
@@ -30,12 +30,18 @@ class ErrorInterceptor : Interceptor {
      * API 결과 코드값에 따른 분기
      */
     private fun parseApiResult(inputStream: InputStream) {
-        val resultCode = XmlParser().parse(inputStream)
+        var resultCode = -1
 
-        when (resultCode.toInt()) {
+        try {
+            resultCode = XmlParser().parse(inputStream).toInt()
+        } catch (e: Exception) {
+            throw Exception(e.message)
+        }
+
+        when (resultCode) {
             SUCCESS.code -> {}
-            NO_RESULT.code -> throw ApiException.NoResultException(NO_RESULT.message)
-            else -> throw Exception()
+            NO_RESULT.code -> throw ServiceException.NoResultException(NO_RESULT.message)
+            else -> throw Exception(ExceptionMessage.NO_SERVICE_RESULT_CODE)
         }
     }
 }
