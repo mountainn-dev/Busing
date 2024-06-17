58e0fe9801c68ddf8e9e5aa5424bc1c5532b490d
seongsan <tjdtks030@naver.com>
2024-06-18 06:03:47 +0900

    rename: 기존 TestFragment 를 SearchStationFragment 로 변경 및 관련 id, 클래스명 수정

--- app/src/main/java/com/san/busing/view/screen/SearchStationFragment.kt
+++ app/src/main/java/com/san/busing/view/screen/SearchStationFragment.kt
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.san.busing.R
 
-class TestFragment : Fragment() {
+class SearchStationFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false)
+        return inflater.inflate(R.layout.fragment_search_station, container, false)
     }
 }
