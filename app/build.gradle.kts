d2533bd8648a094b9ffca3f20dc0d084048e771c
seongsan <tjdtks030@naver.com>
2024-06-30 18:30:20 +0900

    design: 노선 정보 페이지 레이아웃 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ fun getApiKey(key: String): String {
 }
 
 dependencies {
+    implementation("androidx.activity:activity:1.9.0")
     val roomVersion = "2.6.1"
 
     implementation("androidx.core:core-ktx:1.12.0")
---
+++ app/src/main/java/com/san/busing/view/screen/RouteInfoActivity.kt
@@ -0,0 +1,21 @@
+package com.san.busing.view.screen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.san.busing.R
+
+class RouteInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_route_info)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
