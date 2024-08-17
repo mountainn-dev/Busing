4ee88f50de23eb42aaecb9b81e77ea8fcffd3223
seongsan <tjdtks030@naver.com>
2024-08-17 15:33:11 +0900

    fix: 프레그먼트 onStart 에서 노선 검색 키워드가 재설정됨에 따라 불필요하게 발생하던 검색 작업 제거

--- app/src/main/java/com/san/busing/view/screen/SearchRouteFragment.kt
+++ app/src/main/java/com/san/busing/view/screen/SearchRouteFragment.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -190,7 +192,7 @@ class SearchRouteFragment : Fragment() {
     }
 
     private fun setEdRouteListener() {
-        binding.edRoute.doOnTextChanged { text, start, before, count ->
+        binding.edRoute.doAfterTextChanged { text ->
             viewModel.search(text.toString())
         }
     }
@@ -235,7 +237,6 @@ class SearchRouteFragment : Fragment() {
     }
 
     private fun restore() {
-        binding.edRoute.setText(viewModel.keyword)
         viewModel.restore()
     }
 
