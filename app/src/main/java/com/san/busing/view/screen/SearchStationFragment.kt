573dcee3d580ce114cd30285bf933d1b2c3eb719
seongsan <tjdtks030@naver.com>
2024-08-20 22:58:51 +0900

    design: 정류장 검색 결과 아이템 레이아웃 추가

--- app/src/main/java/com/san/busing/view/screen/SearchStationFragment.kt
+++ app/src/main/java/com/san/busing/view/screen/SearchStationFragment.kt
@@ -6,9 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.san.busing.R
+import com.san.busing.databinding.FragmentSearchStationBinding
 
 class SearchStationFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
