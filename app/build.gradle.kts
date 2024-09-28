4b852b8800b85bbb5e2798a7874c9dbf68d16d80
seongsan <tjdtks030@naver.com>
2024-09-29 08:10:18 +0900

    document: README 버전 업데이트 내용 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.san.busing"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
